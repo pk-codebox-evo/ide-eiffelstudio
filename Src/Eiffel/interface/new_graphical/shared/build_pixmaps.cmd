2860cf1296d3eaff46f8ff91443d755cd71c51d8
patrickr <patrickr@8089f293-4706-0410-a29e-feb5c42a2edf>
2006-10-03 22:32:27 +0000

    added script to update pixmap classes fixed reversed pixmap for feature creators/creations

---
+++ Src/Eiffel/interface/new_graphical/shared/build_pixmaps.cmd
@@ -0,0 +1,4 @@
+emcgen -frame J:\57dev\tools\eiffel_matrix_code_generator\frames\studio.e.frame J:\57dev\Delivery\studio\bitmaps\png\10x10.ini
+emcgen -frame J:\57dev\tools\eiffel_matrix_code_generator\frames\studio.e.frame J:\57dev\Delivery\studio\bitmaps\png\12x12.ini
+emcgen -frame J:\57dev\tools\eiffel_matrix_code_generator\frames\studio.e.frame J:\57dev\Delivery\studio\bitmaps\png\16x16.ini
+emcgen -frame J:\57dev\tools\eiffel_matrix_code_generator\frames\studio.e.frame J:\57dev\Delivery\studio\bitmaps\png\cursors.ini
