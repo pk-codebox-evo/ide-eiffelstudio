de9115811d5e019edc052d42d4c113d9a65f42d3
patrickr <patrickr@8089f293-4706-0410-a29e-feb5c42a2edf>
2006-10-03 22:44:37 +0000

    fixed remaining image links

--- Delivery/xmldoc/images/eiffelstudio/icons/generate_icons.cmd
+++ Delivery/xmldoc/images/eiffelstudio/icons/generate_icons.cmd
@@ -1,11 +1,7 @@
-rd /s /q 10x10
-rd /s /q 12x12
-rd /s /q 16x16
-rd /s /q cursors
-mkdir 10x10
-mkdir 12x12
-mkdir 16x16
-mkdir cursors
+del /q 10x10\*
+del /q 12x12\*
+del /q 16x16\*
+del /q cursors\*
 set PNGDIR="%EIFFEL_SRC%/Delivery/studio/bitmaps/png/"
 emcgen -slice "%PNGDIR%/10x10.png" "%PNGDIR%/10x10.ini" -pngs 10x10
 emcgen -slice "%PNGDIR%/12x12.png" "%PNGDIR%/12x12.ini" -pngs 12x12
