53d4c448c981081f4f7f30853c558aba27a22706
aleitner <aleitner@8089f293-4706-0410-a29e-feb5c42a2edf>
2007-11-28 15:03:10 +0000

    removed capture replay from cdd branch

--- cdd/Eiffel_60/examples/capture_replay/iteration0/prepare_c.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-pushd .
-cd EIFGENs/iteration0_example/W_code/C1
-sed -i 's/\([[:space:]]*(FUNCTION_CAST(void, (EIF_REFERENCE, EIF_REFERENCE, EIF_POINTER, EIF_INTEGER_32)) RTVF([[:digit:]]*, [[:digit:]]*, "methodbodystart", [[:alnum:]]*))([[:alnum:]]*, [[:alnum:]]*, \)Current\(, ((EIF_INTEGER_32).*\)/\1c_oitem(0+0 + cargnum + clocnum -1)\2/' *.c
-popd
