e512d9ecbcc2548b4378ff978e225c72f37d92a6
jasonw <jasonw@8089f293-4706-0410-a29e-feb5c42a2edf>
2010-09-28 14:32:59 +0000

    Updated the demo class to use solr index.

--- eve/Src/framework/semantic_search/engine/java/src/Main/Main.java
+++ eve/Src/framework/semantic_search/engine/java/src/Main/Main.java
@@ -4,142 +4,91 @@
  */
 
 package Main;
-import SemanticDocument.SemanticDocument;
-import SemanticDocument.SemanticDocumentLoader;
-import SemanticDocument.SemanticIndexWriter;
-import SemanticDocument.SemanticToLuceneDocumentTranslator;
 import java.io.File;
-import javax.management.Query;
 
-import org.apache.lucene.*;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.document.NumericField;
-import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.Term;
-import org.apache.lucene.queryParser.QueryParser;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.RAMDirectory;
 import org.apache.lucene.util.Version;
 import org.apache.lucene.search.*;
+import org.apache.lucene.search.Sort;
+import org.apache.lucene.search.SortField;
 import org.apache.lucene.search.BooleanClause.Occur;
 import org.apache.lucene.store.FSDirectory;
 
-/**
- *
- * @author jasonw
- */
 public class Main {
 
+    public Directory directory;
+    public Main(String indexDir) {
+        try {
+         directory = FSDirectory.open (new File(indexDir));
+        } catch (Exception e) {
+            
+        }
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-		try{
-                    //SemanticIndexWriter sWriter = new SemanticIndexWriter();
-                    //sWriter.createIndex("d:\\temp\\transitions", "d:\\temp\\index");
+            try{
+                // Initialize the index.
+                Main m = new Main("D:\\apps\\solr\\example\\solr\\data\\index");
 
-		    StandardAnalyzer analyzer = new StandardAnalyzer(Version.LUCENE_30);
+                // Query for LINKED_LIST objects whose count is larger than 8, and whose cursor is_before
+                BooleanQuery query1 = new BooleanQuery();
+                query1.add(new BooleanClause(new TermQuery (new Term ("document_type", "object")), Occur.MUST));
+                query1.add(new BooleanClause(new TermQuery (new Term (m.escapedString("b_prop_{LINKED_LIST [ANY]}.before"), "T")), Occur.MUST));
+                query1.add(new BooleanClause(NumericRangeQuery.newIntRange(m.escapedString ("i_prop_{LINKED_LIST [ANY]}.count"), 2, 20, true, true), Occur.MUST));
+                m.executeQuery(query1);
 
-		    // Store the index in memory:
-		    Directory directory = FSDirectory.open (new File("d:\\temp\\index"));
+                // Query for LINKED_LIST objects whose count is larger than 8, and whose cursor is_before
+                BooleanQuery query2 = new BooleanQuery();
+                query2.add(new BooleanClause(new TermQuery (new Term ("document_type", "transition")), Occur.MUST));
+                query2.add(new BooleanClause(NumericRangeQuery.newIntRange(m.escapedString ("i_by_{LINKED_LIST [ANY]}.count"), 1, 1, true, true), Occur.MUST));
+                m.executeQuery(query2);
+            }catch(Exception e) {
+            }
+	}
 
-//		    IndexWriter iwriter = new IndexWriter(directory, analyzer, true, new IndexWriter.MaxFieldLength(25000));
-//
-//		    // Create a document representing `extend'.
-//		    Document doc = null;
-//
-//		    doc = new Document();
-//		    doc.add(new Field ("post::{LINKED_LIST}.isfirst", "True", Field.Store.NO, Field.Index.NOT_ANALYZED, Field.TermVector.NO));
-//		    doc.add(new Field ("post::{LINKED_LIST}.is_empty", "False", Field.Store.NO, Field.Index.NOT_ANALYZED, Field.TermVector.NO));
-//		    doc.add(new Field ("content", "start", Field.Store.YES, Field.Index.NOT_ANALYZED, Field.TermVector.NO));
-//		    iwriter.addDocument(doc);
-//
-//		    doc = new Document();
-//		    doc.add(new Field ("post::{LINKED_LIST}.has({ANY})", "True", Field.Store.NO, Field.Index.NOT_ANALYZED, Field.TermVector.NO));
-//		    doc.add(new Field ("post::{LINKED_LIST}.is_empty", "False", Field.Store.NO, Field.Index.NOT_ANALYZED, Field.TermVector.NO));
-//		    doc.add(new Field ("content", "put_first", Field.Store.YES, Field.Index.NOT_ANALYZED, Field.TermVector.NO));
-//		    iwriter.addDocument(doc);
-//
-//		    doc = new Document();
-//		    doc.add(new Field ("post::{LINKED_LIST}.has({ANY})", "True", Field.Store.NO, Field.Index.NOT_ANALYZED, Field.TermVector.NO));
-//		    doc.add(new Field ("post::{LINKED_LIST}.is_empty", "False", Field.Store.NO, Field.Index.NOT_ANALYZED, Field.TermVector.NO));
-//		    doc.add(new Field ("post::{LINKED_LIST}.last", "{ANY}", Field.Store.NO, Field.Index.NOT_ANALYZED, Field.TermVector.NO));
-//		    doc.add(new Field ("content", "extend", Field.Store.YES, Field.Index.NOT_ANALYZED, Field.TermVector.NO));
-//		    iwriter.addDocument(doc);
-//
-//		    doc = new Document();
-//		    doc.add(new Field ("post::{LINKED_LIST}.has({ANY})", "True", Field.Store.NO, Field.Index.NOT_ANALYZED, Field.TermVector.NO));
-//		    doc.add(new Field ("post::{1}.  has({3})", "True", Field.Store.NO, Field.Index.NOT_ANALYZED, Field.TermVector.NO));
-//		    doc.add(new Field ("content", "foo", Field.Store.YES, Field.Index.NOT_ANALYZED, Field.TermVector.NO));
-//		    NumericField nf;
-//		    nf = new NumericField ("count");
-//		    nf.setIntValue(1);
-//		    doc.add(nf);
-//		    nf = new NumericField ("count");
-//		    nf.setIntValue(2);
-//		    doc.add(nf);
-//		    iwriter.addDocument(doc);
-//
-//		    iwriter.close();
-            // Query for the document.
-            // Now search the index:
+    // Execute query.
+    public void executeQuery (BooleanQuery query) {
+	try{
+
             IndexSearcher isearcher = new IndexSearcher(directory, true); // read-only=true
 
-            //Term term1 = new Term("post::{LINKED_LIST}.is_empty", "False");
-            //Term term2 = new Term("post::{LINKED_LIST}.has({ANY})" , "True");
-            //Term term3 = new Term("post::{LINKED_LIST}.last" , "{ANY}");
-            
-
-            //Term term1 = new Term("post::{LINKED_LIST}.is_empty", "False");
-            //Term term2 = new Term("post::{1}.  has({3})" , "True");
-
-            Term term1 = new Term ("pre::LINKED_LIST [ANY].is_empty", "False");
-            Term term2 = new Term("to::LINKED_LIST [ANY].after" , "True");
-            Term term3 = new Term("pre::LINKED_LIST [ANY].after" , "False");
-
-
-            TermQuery tq1 = new TermQuery(term1);
-            TermQuery tq2 = new TermQuery(term2);
-            TermQuery tq3 = new TermQuery(term3
-
-            NumericRangeQuery nq = NumericRangeQuery.newIntRange("by::LINKED_LIST [ANY].count", 1, 1, true, true);
-
-		    
-
-            BooleanQuery query = new BooleanQuery();
-            query.add(new BooleanClause(tq1, Occur.SHOULD));
-            //query.add(new BooleanClause(tq2, Occur.MUST));
-            //query.add(new BooleanClause(tq3, Occur.MUST));
-            query.add(new BooleanClause(nq, Occur.MUST));
-
-            //query.add(new BooleanClause(new TermQuery(term1), Occur.SHOULD));
-            //query.add(new BooleanClause(new TermQuery(term2), Occur.SHOULD));
-            //query.add(new BooleanClause(trq, Occur.SHOULD));
-            //query.add(new BooleanClause(new TermQuery(term3), Occur.SHOULD));
-
-            ScoreDoc[] hits = isearcher.search(query, null, 50).scoreDocs;
-
+            ScoreDoc[] hits = isearcher.search(query, null, 5000).scoreDocs;
+            System.out.println("-----------------------------------------------------------------------------------------");
             for(int i=0; i < hits.length; i++) {
                 Document hitDoc = isearcher.doc(hits[i].doc);
                 System.out.print(hits[i].score);
                 System.out.print("\t");
                 System.out.println(hitDoc.getField("content").stringValue());
             }
-
+            System.out.println(hits.length);
             isearcher.close();
-            directory.close();
 
-        }catch(Exception e)
-
+        }catch(Exception e) {
+            System.out.println("Error");
+            System.out.println (e.getMessage());
         }
 
         System.out.println("Finished. OK");
     }
 
+    public String escapedString (String s) {
+        String r1 = s.replaceAll(" ", "%20").
+                      replaceAll("\\[", "%5B").
+                      replaceAll("\\]", "%5D").
+                      replaceAll("\\{", "%7B").
+                      replaceAll("\\}", "%7D").
+                      replaceAll("<", "%%3C").
+                      replaceAll(">", "%%3E").
+                      replaceAll("/", "%%2F").
+                      replaceAll("\\+", "%%2B").
+                      replaceAll("\\-", "%%2D").
+                      replaceAll("~", "%%7E");
+        System.out.println(r1);
+        return r1;
+    }
 }
